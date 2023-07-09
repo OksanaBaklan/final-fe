b809572e0450f472516f950bf78efab5ab81e9ac
Oksana <your@email.com>
2023-07-09 12:23:35 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import "./App.css";
+import AppBackground from "./components/AppBackground/AppBackground";
+
+function App() {
+  return (
+    <div className="App">
+      <AppBackground>
+        <h1>Money Minder</h1>{" "}
+      </AppBackground>
+    </div>
+  );
+}
+
+export default App;
