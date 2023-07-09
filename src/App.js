ff36468ce0dece388fc31f9f9393b27afbd4d2b7
Oksana <your@email.com>
2023-07-09 14:07:01 +0200

    added /loginPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 /** @format */
 
+import { useSelector } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import AppBackground from "./components/AppBackground/AppBackground";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
     <div className="App">
       <AppBackground>
         <h1>Money Minder</h1>{" "}
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
       </AppBackground>
     </div>
   );
