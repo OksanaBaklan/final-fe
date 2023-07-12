cd2bba1c064a2e6090fba54b49318334843d1ba7
Oksana <your@email.com>
2023-07-12 12:05:07 +0200

    added register verify pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 /** @format */
 
-import { useSelector } from "react-redux";
+import { useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import AppBackground from "./components/AppBackground/AppBackground";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import VerifyPage from "./pages/VerifyPage/VerifyPage";
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [userId, setUserId] = useState("");
+  const [avatar, setAvatar] = useState("")
+
   return (
     <div className="App">
       <AppBackground>
-        <h1>Money Minder</h1>{" "}
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage  setAuthenticated={setAuthenticated}
+              setUserName={setUserName}
+              setUserId={setUserId}
+              authenticated={authenticated}
+              setAvatar = {setAvatar}/>} />
+              <Route
+                  path="/register"
+                  element={authenticated ? <Navigate to="/" /> : <RegisterPage />}
+                />
+                              <Route
+                  path="/verify/:verificationToken"
+                  element={<VerifyPage/>}
+                />
         </Routes>
       </AppBackground>
     </div>
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,8 +1,13 @@
+/** @format */
+
 import s from "./LoginPage.module.css";
 import { useMediaQuery } from "react-responsive";
 import LoginForm from "../../components/LoginForm";
 
-function LoginPage() {
+function LoginPage(props) {
+  const { setAuthenticated, setUserName, setUserId, authenticated, setAvatar } =
+    props;
+
   const isLarge = useMediaQuery({ query: "(min-width: 1280px)" });
   return (
     <>
@@ -12,7 +17,13 @@ function LoginPage() {
           <h1 className={s.title}>Finance App</h1>
         </div>
         <div className={s.content_container}>
-          <LoginForm />
+          <LoginForm
+            setAuthenticated={setAuthenticated}
+            setUserName={setUserName}
+            setUserId={setUserId}
+            authenticated={authenticated}
+            setAvatar={setAvatar}
+          />
         </div>
       </div>
     </>
