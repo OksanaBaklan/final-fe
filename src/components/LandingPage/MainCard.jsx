a11587dc5a7022cac404a9f8046ea065c0ac3bea
Oksana <your@email.com>
2023-08-24 14:35:22 +0200

    css for mob and tablet landing page

--- src/components/LandingPage/MainCard.jsx
+++ src/components/LandingPage/MainCard.jsx
@@ -5,7 +5,7 @@ const MainCard = () => {
   return (
     <div className='main-card'>
     <h1>
-      Welcome to Money Minder Your Financial Tracking Companion
+      Welcome to Money Minder <br/> Your Financial Tracking Companion
     </h1>
     <p>Simplify your financial life , the ultimate app  for tracking transactions and expenses.</p>
   </div>
