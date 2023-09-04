2265b1b83ac7ad7baaea0cd0bd56cc624d841c92
Oksana <your@email.com>
2023-09-04 10:53:00 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 /** @format */
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import UserProvider from "./storeContext/UserContext";
-import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import UserProvider from './storeContext/UserContext';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-    <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <UserProvider>
-        <App />
-      </UserProvider>
-      </PersistGate>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <UserProvider>
+            <App />
+          </UserProvider>
+        </PersistGate>
       </Provider>
     </React.StrictMode>
-  </BrowserRouter>,
+  </BrowserRouter>
 );
-
 
 reportWebVitals();
